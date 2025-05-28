fe4a2627bc94cd3aa4c511b559ae329e67fb3dcf
Mithat <mithatsirmali@gmail.com>
2025-05-28 16:37:29 +0300

    Add files via upload

---
+++ Enum ve Struct2/Enum ve Struct2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_ve_Struct2
+{
+    internal class Program
+    {
+        // Enum: Departman Tanımları
+        enum Departman
+        {
+            Yazilim,
+            Muhasebe,
+            InsanKaynaklari,
+            Pazarlama
+        }
+
+        // Struct: Personel Bilgileri
+        struct Personel
+        {
+            public int ID;
+            public string AdSoyad;
+            public Departman Departman;
+
+            public void BilgiYazdir()
+            {
+                Console.WriteLine($"ID: {ID}, Ad: {AdSoyad}, Departman: {Departman}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Personel p1 = new Personel();
+            p1.ID = 1;
+            p1.AdSoyad = "Zeynep Kaya";
+            p1.Departman = Departman.Yazilim;
+
+            Personel p2 = new Personel
+            {
+                ID = 2,
+                AdSoyad = "Mehmet Demir",
+                Departman = Departman.Muhasebe
+            };
+
+            p1.BilgiYazdir();
+            p2.BilgiYazdir();
+        }
+    }
+}
